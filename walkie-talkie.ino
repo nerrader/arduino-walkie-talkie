15b62e49f9794545cc8c888b695c36a33ecdf160
nerrader <nerrader@proton.me>
2026-05-06 17:00:47 +0700

    added debug messages hopefully fixed lcd thing

--- walkie-talkie.ino
+++ walkie-talkie.ino
@@ -36,8 +36,11 @@ const char* upperLayout[] = {" 0", ".,1", "abc2", "def3", "ghi4", "jkl5", "mno6"
 bool isCapsLock = false;
 
 void setup() {
+
+  const int baudRate = 9600
   Wire.begin(); // apparently this is for the i2c or lcd screen idk how it works but it does
-  HC12.begin(9600);
+  HC12.begin(baudRate);
+  Serial.begin(baudRate)
   lcd.init();
   lcd.backlight();
   lcd.print("the walkie talkie is ready sir");
@@ -51,6 +54,7 @@ int sameKeyTaps = 0;
 
 void loop() {
   if (HC12.available()) {
+    Serial.println("HC12 is available")
     lcd.clear();
     lcd.setCursor(0, 0);
 
@@ -64,6 +68,11 @@ void loop() {
   char key = customKeypad.getKey();
   // if in the 0-9 keys
 
+  if (!key){
+    return;
+  }
+
+  Serial.println(key)
   if (key >= '0' && key <= '9') {
     int intKey = key - '0';
     if (intKey == lastKey || lastKey == -1){ //same key or first key you pressed
@@ -80,17 +89,21 @@ void loop() {
   // if not in the 0-9 keys
 
   else {
+    Serial.println(key)
     switch (key){
-
       case 'A': //toggles on and off caps lock pretty much
+        Serial.println("Reached A block")
         isCapsLock = !isCapsLock;
         break;
 
       case 'B':
+        Serial.println("Reached B Block")
         break;
 
       case 'C': // convert sameKeyTaps to char
         // double pointer because apprantly strings are just lists of chars, and lists are pointers so
+        Serial.println("Reached C Block")
+        if (sameKeyTaps == 0 || lastKey == -1) break;
         const char** currentLayout = isCapsLock ? upperLayout : lowerLayout;
         int index = (sameKeyTaps - 1) % strlen(currentLayout[lastKey]); // so if you press it 5 times, it goes back to the first char
         currentMessage += currentLayout[lastKey][index];
@@ -102,6 +115,7 @@ void loop() {
         break;
 
       case 'D': // destroy, or clear the entire thing
+        Serial.println("Reached D Block")
         currentMessage = "";
         visibleMessage = currentMessage;
         sameKeyTaps = 0;
@@ -110,6 +124,7 @@ void loop() {
         break;
 
       case '*': //send
+        Serial.println("Reached * Block")
         HC12.print(currentMessage);
         currentMessage = "";
         visibleMessage = "";
@@ -117,8 +132,11 @@ void loop() {
         sameKeyTaps = 0;
         lcd.clear();
         lcd.print("Message sent!");
+        delay(500);
         break;
+
       case '#': // backspace
+        Serial.println("Reached # Block")
         if (sameKeyTaps > 0) { //basically if the user has number keys pressed
           visibleMessage.remove(visibleMessage.length() - 1);
           sameKeyTaps--;
@@ -138,7 +156,22 @@ void loop() {
 }
 
 void refreshLCD(){
+  Serial.println("Refreshing the LCD")
   lcd.clear();
+
   lcd.setCursor(0, 0);
-  lcd.print(visibleMessage);
+  lcd.print(visibleMessage.substring(0, 20));
+
+  if (visibleMessage.length() > 20){
+    lcd.setCursor(0, 1);
+    lcd.print(visibleMessage.substring(20, 40));
+  }
+  if (visibleMessage.length() > 40){
+    lcd.setCursor(0, 2);
+    lcd.print(visibleMessage.substring(40, 60));
+  }
+  if (visibleMessage.length() > 60){
+    lcd.setCursor(0, 3);
+    lcd.print(visibleMessage.substring(60));
+  }
 }
